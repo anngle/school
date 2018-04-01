b9f1c1909a3382c9882608cbed67b8d4be38b8e4
anaf <anaf@163.com>
2018-04-01 12:37:39 +0800

    找到问题了

--- school/wx/views.py
+++ school/wx/views.py
@@ -2,10 +2,42 @@
 from flask import Blueprint, render_template, url_for, current_app, redirect,request,flash
 from flask_wechatpy import wechat_required
 from wechatpy.replies import TextReply,ArticlesReply,create_reply,ImageReply
-# from school.extensions import wechat
 from school.extensions import csrf_protect
 
 blueprint = Blueprint('wx', __name__, url_prefix='/wechat')
+
+
+
+def createmenu():
+    flask_wechat.menu.create({"button":[
+        {"type":"view","name":u"请假","sub_button":[
+            {
+                "type":"view",
+                "name":u"我要请假",
+                "url":'http://car.anaf.cn/usercenter/show_order'
+            },
+            {
+                "type":"view",
+                "name":u"代请假",
+                "url":'http://car.anaf.cn/driver/add_post'
+            },
+            ]},\
+        
+        {"type":"view","name":u"用户服务","sub_button":[
+            
+            {
+                "type":"view",
+                "name":u"平台简介",
+                "key":'contact_us'
+            },
+            {
+                "type":"view",
+                "name":u"个人中心",
+                "url":'http://car.anaf.cn/usercenter'
+            },
+            ]},\
+        ]})
+
 
 
 #微信获取token
@@ -30,15 +62,28 @@ def token_get():
 
 
 
-#微信调用 位置和token等其他相关
+#微信token信息提交
 @csrf_protect.exempt
 @blueprint.route('/token',methods=['POST'])
 @wechat_required
 def token_post():
-    print 'jinru'
-    # msg = request.wechat_msg
-    print  'return'
-    return 'reply'
+    msg = request.wechat_msg
+    reply=''
+    if msg.type == 'text':
+        reply=TextReply(content=u'哈哈哈哈或或恍恍惚惚', message=msg)
+
+    #关注事件
+    if msg.event == 'subscribe':
+        createmenu()
+        reply = TextReply(content=u'欢迎关注。O(∩_∩)O哈！', message=msg)
+    #扫描二维码关注事件
+    if msg.event == 'subscribe_scan':
+        createmenu()
+        reply = TextReply(content=u'欢迎关注。O(∩_∩)O哈！', message=msg)
+
+
+    
+    return reply
     
 
 
