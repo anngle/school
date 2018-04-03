0d03a9eac2a24a59d533259eea88549237fb0de0
anaf <anaf@163.com>
2018-04-04 01:43:24 +0800

    test wechat msg

--- school/user/views.py
+++ school/user/views.py
@@ -151,6 +151,8 @@ def send_leave_post():
 
 	doork = student_role = Role.query.filter_by(name='Doorkeeper').first()
 	#如果不是门卫 则则判断其他角色
+	print current_user.roles
+	print dork.name
 	if not current_user.roles!=doork:	
 		student_role = Role.query.filter_by(name='Students').first()
 		if current_user.roles==student_role:
